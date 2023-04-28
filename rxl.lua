9ce63ad75b1f8ff5b27b3f4ca2bfa03f584eaf20
Zenxdkd <99894254+Zenxdkd@users.noreply.github.com>
2023-04-28 18:51:57 +0200

    Update rxl.lua

--- rxl.lua
+++ rxl.lua
@@ -1,5 +1,3 @@
-
-
 local function getnamecall()
     if game.PlaceId == 2788229376 then
         return "UpdateMousePos"
@@ -23,16 +21,15 @@ end
 local mainevent = MainEventLocate()
 
 -- // Shorthand
-local uwuRXL = getgenv().RXL
-local uwuMain = uwuRXL.General
-local uwuCamMain = uwuRXL.Camlock.Main
-local uwuCamFOV = uwuRXL.Camlock.FOV
-local uwuSilentMain = uwuRXL.Silent.Main
-local uwuSilentFOV = uwuRXL.Silent.FOV
-local uwuTrace = uwuRXL.Tracer
-local uwuFpsUnlock = uwuRXL.Misc.FpsUnlocker
-local uwuHeadless = uwuRXL.Misc.Headless
-local uwu360 = uwuRXL.Key360.Enabled
+local uwuZ4 = getgenv().Z4
+local uwuMain = uwuZ4.General
+local uwuCamMain = uwuZ4.Camlock.Main
+local uwuCamFOV = uwuZ4.Camlock.FOV
+local uwuSilentMain = uwuZ4.Silent.Main
+local uwuSilentFOV = uwuZ4.Silent.FOV
+local uwuTrace = uwuZ4.Tracer
+local uwuFpsUnlock = uwuZ4.Misc.FpsUnlocker
+local uwuHeadless = uwuZ4.Misc.Headless
 
 -- // Optimization
 local vect3 = Vector3.new
@@ -65,7 +62,7 @@ local c = ws.CurrentCamera
 -- // Notification Function
 local function SendNotification(text)
     Notification:Notify(
-        {Title = "RXL Rewrite", Description = "exit#7360 - "..text},
+        {Title = "Z4 Rewrite", Description = "exit#7360 - "..text},
         {OutlineColor = Color3.fromRGB(50,76,110),Time = 2, Type = "image"},
         {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(50,76,110)}
     )
@@ -73,9 +70,9 @@ end
 
 -- // Call notification function
 if uwuMain.Notifications then
-    SendNotification("exit#7360 - injecting RXL Rewrite")
+    SendNotification("exit#7360 - injecting Z4 Rewrite")
     wait(3.5)
-    SendNotification("exit#7360 - finished injecting RXL Rewrite")
+    SendNotification("exit#7360 - finished injecting Z4 Rewrite")
 end
 
 -- // Camlock FOV
@@ -406,46 +403,4 @@ end
 
 if uwuHeadless then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackerProLuaStuff/avatar-editor-thing/main/headless.lua"))()
-end
-
-
--- 360
-
-if uwu360 then
- local Players = game:GetService("Players")
- local UserInputService = game:GetService("UserInputService")
- local RunService = game:GetService("RunService")
- local Camera = workspace.CurrentCamera
- local Toggle = getgenv().RXL.Key360.Toggle
- local RotationSpeed = getgenv().RXL.Key360.RotationSpeed
- local Keybind = getgenv().RXL.Key360.Keybind
- local function OnKeyPress(Input, GameProcessedEvent)
-
-     if Input.KeyCode == Keybind and not GameProcessedEvent then 
-         Toggle = not Toggle
-     end
- end
-
- UserInputService.InputBegan:Connect(OnKeyPress)
- local LastRenderTime = 0
- local FullCircleRotation = 2 * math.pi
- local TotalRotation = 0
-
- local function RotateCamera()
-     if Toggle then
-         local CurrentTime = tick()
-         local TimeDelta = math.min(CurrentTime - LastRenderTime, 0.01)
-         LastRenderTime = CurrentTime
-
-         local Rotation = CFrame.fromAxisAngle(Vector3.new(0, 1, 0), math.rad(RotationSpeed * TimeDelta))
-         Camera.CFrame = Camera.CFrame * Rotation
-
-         TotalRotation = TotalRotation + math.rad(RotationSpeed * TimeDelta)
-         if TotalRotation >= FullCircleRotation then
-             Toggle = false
-             TotalRotation = 0
-         end
-     end
- end
- RunService.RenderStepped:Connect(RotateCamera)
 end
