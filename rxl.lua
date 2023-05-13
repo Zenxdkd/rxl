2a8140f065a967d38f62b7682edb530d0decae13
Zenxdkd <99894254+Zenxdkd@users.noreply.github.com>
2023-05-13 13:17:39 +0200

    Update rxl.lua

--- rxl.lua
+++ rxl.lua
@@ -34,6 +34,7 @@ local uwu360 = uwuZ4.Key360.Enabled
 local uwuMacro = uwuZ4.Macro.Enabled
 local uwuMacroKeyBind = uwuZ4.Macro.KeyBind
 local uwuSkin = uwuZ4.Misc.SkinChanger
+local uwuUnlockOnDeath = uwuZ4.Misc.UnlockOnDeath
 
 -- // Optimization
 local vect3 = Vector3.new
@@ -259,7 +260,7 @@ rs.RenderStepped:Connect(function()
         CPart = uwuFindPart()
         local pos = nil
         local cum = nil
-        if CTarget.Character.BodyEffects["K.O"].Value == true or lplr.Character.BodyEffects["K.O"].Value == true then
+        if uwuUnlockOnDeath and CTarget.Character.BodyEffects["K.O"].Value == true or lplr.Character.BodyEffects["K.O"].Value == true then
             CToggle = false
             CTarget = nil
         else
@@ -412,6 +413,7 @@ end
 if uwuSkin then -- skin changer
     loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\73\110\118\111\111\107\101\114\49\49\47\79\117\116\102\105\116\47\109\97\105\110\47\79\117\116\102\105\116\67\111\112\105\101\114\46\108\117\97\34\44\32\116\114\117\101\41\41\40\41\10")()
 end
+
 
 
 --360 
